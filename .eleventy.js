ad7ee0744ac3010a163703dfcdf3fb10e3c56b6f
uhyo <uhyo@uhy.ooo>
2022-02-05 03:13:17 +0900

    minification of 11ty output

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,40 @@
+const htmlmin = require("html-minifier");
+const CleanCSS = require("clean-css");
+const { minify } = require("terser");
+
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("emojis/*.png")
   eleventyConfig.addPassthroughCopy("ogp/ogp.png")
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+  eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+    code,
+    callback
+  ) {
+    try {
+      const minified = await minify(code);
+      callback(null, minified.code);
+    } catch (err) {
+      console.error("Terser error: ", err);
+      callback(null, code);
+    }
+  });
+
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if (this.outputPath && this.outputPath.endsWith(".html")) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     dir: {
